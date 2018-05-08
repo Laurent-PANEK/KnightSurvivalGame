10e036c040d54e91f10f1a0f5d6811526d4f4948
Abdessalam98 <belmehelabdessalam.benharira@ynov.com>
2018-05-08 19:22:59 +0200

    [Add] Attack player

--- Assets/PlayerControll.cs
+++ Assets/PlayerControll.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerControll : MonoBehaviour {
+
+	public float speed = 3.0f;
+	public float rotationSpeed = 150.0f;
+	public int damage = 20;
 	void Update () {
-		var x = Input.GetAxis ("Horizontal") * Time.deltaTime * 150.0f;
-		var z = Input.GetAxis ("Vertical") * Time.deltaTime * 3.0f;
 
-		transform.Rotate (0, x, 0);
-		transform.Translate (0, 0, z);
-
+		float translation = Input.GetAxis ("Vertical") * speed;
+		float rotation = Input.GetAxis ("Horizontal") * rotationSpeed;
+		RaycastHit hit;
+		translation *= Time.deltaTime;
+		rotation *= Time.deltaTime;
+		transform.Translate (0, 0, translation);
+		transform.Rotate (0, rotation, 0);
 		if (Input.GetKeyDown ("space")) {
-			GameObject.Find ("Dragonblade").GetComponent<Animation> ().Play ("Attack");
+			if (Physics.Raycast (transform.position, transform.TransformDirection (Vector3.forward), out hit)) {
+				if (hit.distance <= 2) {
+					GameObject.Find ("Dragonblade").GetComponent<Animation> ().Play ("Attack");
+					hit.transform.SendMessage ("ApplyDamage", damage, SendMessageOptions.DontRequireReceiver);
+				}
+			}
 		}
 	}
 }
