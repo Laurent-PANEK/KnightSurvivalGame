4bf06125c9dc837b8bbb76f3ed7886c1e594c8f0
Abdessalam98 <belmehelabdessalam.benharira@ynov.com>
2018-05-08 14:30:56 +0200

    [Add] player

---
+++ Assets/AttackMethod.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackMethod : MonoBehaviour {
+	public Animation anim;
+	public void Start () {
+		anim = GetComponent<Animation> ();
+	}
+
+	public void Update () {
+		if (Input.GetKeyDown ("e")) {
+			anim.Play ("animation");
+		}
+	}
+}
