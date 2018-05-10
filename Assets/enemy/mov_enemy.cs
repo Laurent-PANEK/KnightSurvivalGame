a202a93381762c17ba4a14ae40322a8041118f1f
Laurent-PANEK <laurent.panek@ynov.com>
2018-05-10 18:12:42 +0200

    [Added] Enemy : Add Behaviour & Spawn

---
+++ Assets/enemy/mov_enemy.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class mov_enemy : MonoBehaviour {
+
+    public Transform[] Destinations;
+
+	// Use this for initialization
+	void Start () {
+        SetDestination();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        NavMeshAgent nav = GetComponent<NavMeshAgent>();
+        if(nav.remainingDistance < 1)
+        {
+            SetDestination();
+        }
+
+    }
+
+    public void SetDestination()
+    {
+        NavMeshAgent nav = GetComponent<NavMeshAgent>();
+        int r = Random.Range(0, Destinations.Length);
+        nav.SetDestination(Destinations[r].position);
+    }
+
+    public bool CheckAniClip(string clipname)
+    {
+        if (this.GetComponent<Animation>().GetClip(clipname) == null)
+            return false;
+        else if (this.GetComponent<Animation>().GetClip(clipname) != null)
+            return true;
+
+        return false;
+    }
+}
